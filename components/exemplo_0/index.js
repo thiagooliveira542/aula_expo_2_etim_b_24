3853f4946d2f37b0b4c396c42631d30cc0aff9f1
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2024-02-27 15:24:28 -0300

    aula 27-02

---
+++ components/exemplo_0/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'; 
+
+function Exemplo_0 () {
+    return(
+        <View>
+            <Text>Minha primeira tela</Text>
+        </View>
+    );
+}
+
+export default Exemplo_0;
