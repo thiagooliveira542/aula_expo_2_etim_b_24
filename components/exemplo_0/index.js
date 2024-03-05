16ca2974d6499a5b101da41d077fbcdd57efcd38
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2024-03-05 15:25:11 -0300

    aula 05-03

--- components/exemplo_0/index.js
+++ components/exemplo_0/index.js
@@ -1,9 +1,11 @@
 import { View, Text } from 'react-native'; 
 
+import styles from './styles';
+
 function Exemplo_0 () {
     return(
-        <View>
-            <Text style={{color: '#fff'}}>Minha primeira tela</Text>
+        <View style={styles.container}>
+            <Text>Minha primeira tela</Text>
         </View>
     );
 }
---
+++ components/exemplo_0/styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native'; 
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#0f0', 
+        padding: 10, 
+    }, 
+    texto: {
+        color: '#fff', 
+        fontSize: 20, 
+    },
+});
+
+export default styles;
