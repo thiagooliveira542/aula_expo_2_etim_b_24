65a7e12e29068897c22822f358ecae0dd51d4bd3
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2024-03-08 09:27:22 -0300

    aula 08-03

--- components/exemplo_1/index.js
+++ components/exemplo_1/index.js
@@ -5,7 +5,7 @@ import styles from './styles';
 function Exemplo_0 () {
     return(
         <View style={styles.container}>
-            <Text>Minha primeira tela</Text>
+            <Text style={styles.texto}>Minha primeira tela</Text>
         </View>
     );
 }
