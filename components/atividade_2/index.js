dae18587b8046eb4103a98fc2421c39bbf3c9be2
thiagooliveira542 <163539750+thiagooliveira542@users.noreply.github.com>
2024-04-26 09:40:07 -0300

    26/04

--- components/atividade_2/index.js
+++ components/atividade_2/index.js
@@ -1,10 +1,10 @@
-import { View } from "react-native-web";
+import { View, Text } from "react-native";
 import styles from './styles' ;
 
 export default function Atividade_2 () {
     return(
-        <View  styles={styles.container}>
-            <Text  styles={styles.titulo}>
+        <View  style={styles.container}>
+            <Text  style={styles.titulo}>
               Atividade 2
             </Text>
         </View>
