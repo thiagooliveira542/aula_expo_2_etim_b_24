8b2d961379eb22d23be74fa7d070bffd63250d53
thiagooliveira542 <163539750+thiagooliveira542@users.noreply.github.com>
2024-05-10 09:51:30 -0300

    10/05

--- components/exemplo 3/index.js
+++ components/exemplo 3/index.js
@@ -3,11 +3,11 @@ import { View, Text, Pressable } from 'react-native';
 
 import styles from './styles';
 
- function Exemplo_3 () {
+export default function Exemplo_3() {
 
     const [numero, setNumero] = useState(0);
 
-    function incremento () {
+    function incremento() {
         setNumero(numero + 1);
     }
 
@@ -16,27 +16,27 @@ import styles from './styles';
             <Text style={styles.titulo}>Exemplo  3</Text>
 
             <Pressable
-             onPress={() => alert('Oi')}
-              style={({ pressed }) => pressed ? 
-                   [styles.botao, styles.botaoPress]
-                   :
-                   styles.botao}
+                onPress={() => alert('Oi')}
+                style={({ pressed }) => pressed ?
+                    [styles.botao, styles.botaoPress]
+                    :
+                    styles.botao}
             >
                 <Text style={styles.txtBotao}>Ex. botão</Text>
-                <Text style={style.txtNumero}>{numero}</Text>
 
-            <Text style={styles.titulo}>Exemplo State</Text>
-        
+                <Text style={styles.titulo}>Exemplo State</Text>
+            </Pressable>
 
+            <Text style={styles.txtNumero}>{numero}</Text>
             <Pressable
-              onPress={} => incremento()}
-              style={({ pressed  }) => pressed ?
-              [styles.botao, styles.botaoPress]
-              :
-              styles.botao}
-        >
-            <Text style={styles.txtbotao}>+1</Text>
-        </Pressable>
+                onPress={() => incremento()}
+                style={({ pressed }) => pressed ?
+                    [styles.botao, styles.botaoPress]
+                    :
+                    styles.botao}
+            >
+                <Text style={styles.txtbotao}>+1</Text>
+            </Pressable>
 
         </View>
     );
